44cd6860848865a509630f9de6440cf209839eb8
teganasprey <82981420+teganasprey@users.noreply.github.com>
2023-01-08 14:00:37 -0500

    Create CNN.py

---
+++ Models/NeuralNet/CNN.py
@@ -0,0 +1,13 @@
+import numpy as np
+import tensorflow as tf
+from time import time
+import math
+
+#TRAINING
+#import data
+#params
+#loss and optimizer
+#prediction and accuracy calculation
+#saver
+
+#RUN NETWORK ON TEST DATASET
