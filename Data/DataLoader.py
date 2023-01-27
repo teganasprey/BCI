f6529159ee5dc8432479c6231c69c9df3509fd1e
Steven Asprey <s-asprey@users.noreply.github.com>
2023-01-27 14:16:03 -0500

    Update DataLoader.py

--- Data/DataLoader.py
+++ Data/DataLoader.py
@@ -107,7 +107,7 @@ class DataLoader(object):
         experiment_id = self.get_next_experiment_id()
         if experiment_id > 0:
             experiment_query = 'insert into experiment_information ' \
-                               '(experiment_id, experiment_date, paradigm, subject_id, states, stimuli, [mode]) ' \
+                               '(experiment_id, experiment_date, paradigm, subject_id, states, stimuli, mode) ' \
                                'values ' \
                                '('
             experiment_query += str(experiment_id) + ', '
@@ -117,7 +117,7 @@ class DataLoader(object):
             experiment_query += '\'' + self.states + '\', '
             experiment_query += '\'' + self.experiment_stimuli + '\', '
             experiment_query += '\'' + self.experiment_mode + '\')'
-            # postgres.execute(sql_query=experiment_query)
+            postgres.execute(sql_query=experiment_query)
 
             # create a csv file with the data formatted correctly for the signal_data table
             data = self.to_pandas()
@@ -126,18 +126,17 @@ class DataLoader(object):
             columns = data.columns[-2:].tolist() + data.columns[:-2].to_list()
             data_to_file = data[columns]
             filename = ''.join([self.experiment_paradigm, self.file_date, self.subject]) + '.csv'
-            full_filename = self.data_directory
             if self.operating_system == 'Windows':
-                full_filename += '\\' + filename
+                full_filename = 'C:\\Users\\Public\\Downloads\\' + filename
             elif self.operating_system == 'Darwin':
-                full_filename += '/' + filename
-            data_to_file.to_csv(path=full_filename, sep=',')
+                full_filename = '/' + filename
+            data_to_file.to_csv(full_filename, sep=',', index=False)
             data_query = 'COPY signal_data (experiment_id, sample_index, marker, "Fp1", "Fp2", "F3", "F4", "C3", ' \
                          '"C4", "P3", "P4", "O1", "O2", "A1", "A2", "F7", "F8", "T3", "T4", "T5", "T6", "Fz", "Cz", ' \
-                         '"Pz", "X3") ' \
-                         'FROM \'' + filename + '\'' \
-                         'DELIMITER \',\' ' \
-                         'CSV HEADER;'
+                         '"Pz", "X3") '
+            data_query += 'FROM \'' + full_filename + '\' '
+            data_query += 'DELIMITER \',\' '
+            data_query += 'CSV HEADER;'
             postgres.execute(sql_query=data_query)
         else:
             # experiment_id already exists, no need to push the data again
