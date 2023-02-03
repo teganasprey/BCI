d0d5177d6c2b2eb2bb0edb3ceeb35c635f7bb063
Steven Asprey <s-asprey@users.noreply.github.com>
2023-02-03 16:50:55 -0500

    Update DataLoader.py

--- Data/DataLoader.py
+++ Data/DataLoader.py
@@ -8,6 +8,14 @@ import platform
 import mne
 import os
 
+# imports for testing
+import numpy as np
+from sklearn.pipeline import Pipeline
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.model_selection import ShuffleSplit, cross_val_score
+from mne.decoding import CSP
+
+
 if platform.system() == 'Darwin':
     import matplotlib as mpl
     import matplotlib.pyplot as plt
@@ -319,16 +327,16 @@ if __name__ == '__main__':
     # plot the raw data with events overlaid
     # raw_mne.plot(events=events, color='gray', event_color=dl.CLA_HALT_FREEFORM_EVENT_COLORS, scalings='auto')
 
-    # calculate and plot the power spectrum
-    # raw_mne.plot_psd(average=True)
+    # try some filtering
+    raw_filter = raw_mne.copy()
+    raw_filter.filter(7., 30., fir_design='firwin', skip_by_annotation='edge', picks='eeg')
 
-    # other tests to run:
+    # other data format tests to run:
     # raw_mne_file = dl.to_mne_raw()
     # dfd = dl.to_pandas()
     # dfl = dl.to_polars()
 
     # create epochs
-    # epochs = mne.Epochs(raw_mne, events, tmin=-0.3, tmax=0.7)
     event_dict = {'left hand MI': 1, 'right hand MI': 2, 'passive state': 3,
                   'initial relaxation period': 99, 'inter-session rest break period': 91,
                   'experiment end': 92}
@@ -336,6 +344,71 @@ if __name__ == '__main__':
                         preload=True)
     evoked_lh = epochs['left hand MI'].average()
     evoked_rh = epochs['right hand MI'].average()
+
+    # set aside training data for the CSP
+    epochs_train = epochs.copy().crop(tmin=1., tmax=2.)
+    labels = epochs.events[:, -1] - 2
+
+    # define a monte-carlo cross-validation generator (reduce variance):
+    epochs_data = epochs.get_data()
+    epochs_data_train = epochs_train.get_data()
+    cv = ShuffleSplit(10, test_size=0.2, random_state=42)
+    cv_split = cv.split(epochs_data_train)
+
+    # assemble a classifier
+    lda = LinearDiscriminantAnalysis()
+    csp = CSP(n_components=4, reg=None, log=True, norm_trace=False)
+
+    # Use scikit-learn Pipeline with cross_val_score function
+    clf = Pipeline([('CSP', csp), ('LDA', lda)])
+    scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=1)
+
+    # Printing the results
+    class_balance = np.mean(labels == labels[0])
+    class_balance = max(class_balance, 1. - class_balance)
+    print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores), class_balance))
+
+    # plot CSP patterns estimated on full data for visualization
+    csp.fit_transform(epochs_data, labels)
+    csp.plot_patterns(epochs.info, ch_type='eeg', units='Patterns (AU)', size=1.5)
+
+    sfreq = raw_mne.info['sfreq']
+    w_length = int(sfreq * 0.5)  # running classifier: window length
+    w_step = int(sfreq * 0.1)  # running classifier: window step size
+    w_start = np.arange(0, epochs_data.shape[2] - w_length, w_step)
+
+    scores_windows = []
+
+    for train_idx, test_idx in cv_split:
+        y_train, y_test = labels[train_idx], labels[test_idx]
+
+        X_train = csp.fit_transform(epochs_data_train[train_idx], y_train)
+        X_test = csp.transform(epochs_data_train[test_idx])
+
+        # fit classifier
+        lda.fit(X_train, y_train)
+
+        # running classifier: test classifier on sliding window
+        score_this_window = []
+        for n in w_start:
+            X_test = csp.transform(epochs_data[test_idx][:, :, n:(n + w_length)])
+            score_this_window.append(lda.score(X_test, y_test))
+        scores_windows.append(score_this_window)
+
+    # Plot scores over time
+    w_times = (w_start + w_length / 2.) / sfreq + epochs.tmin
+
+    plt.figure()
+    plt.plot(w_times, np.mean(scores_windows, 0), label='Score')
+    plt.axvline(0, linestyle='--', color='k', label='Onset')
+    plt.axhline(0.5, linestyle='-', color='k', label='Chance')
+    plt.xlabel('time (s)')
+    plt.ylabel('classification accuracy')
+    plt.title('Classification score over time')
+    plt.legend(loc='lower right')
+    plt.show()
+
+    # visualize Epochs
     # epochs['left hand MI'].plot_psd(picks='eeg')
     # epochs['left hand MI'].plot_psd_topomap()
     # epochs['left hand MI'].plot_image(picks='eeg', combine='mean')
